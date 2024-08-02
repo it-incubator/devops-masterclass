90a2afdd57c16897711fcb01a21ccdb7a39e2d26
abazunts <bazunc@gmail.com>
2024-08-02 18:46:30 +0400

    lesson 6

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,15 @@
 const http = require('http');
 
 const hostname = '0.0.0.0';
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 8000;
 
 const app = (req, res) => {
     res.statusCode = 200;
+
+    // Получаем IP адрес пода из переменной окружения
+    const podIP = process.env.MY_POD_IP;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!\n');
+    res.end(`Hello, World!\nPod IP: ${podIP}\n`);
 };
 
 let server;
